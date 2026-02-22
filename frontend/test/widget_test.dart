1f486d7d9f7c9071dd50b9a54929ceebb5cb35d8
williansalceda <willian@salceda.com.br>
2026-02-22 13:57:25 -0300

    fix(frontend): update widget_test for DI implementation

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:real_estate_app/app.dart';
+import 'package:real_estate_app/core/di/injection.dart';
 
 void main() {
   testWidgets('RealEstateApp smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      RealEstateApp(
-        apiBaseUrl: 'http://localhost:8080',
-        locale: const Locale('en'),
-      ),
+    await sl.reset();
+    await configureDependencies(
+      apiBaseUrl: 'http://localhost:8080',
+      locale: const Locale('en'),
     );
+
+    await tester.pumpWidget(const RealEstateApp());
 
     expect(find.text('Real Estate Ads'), findsOneWidget);
   });
